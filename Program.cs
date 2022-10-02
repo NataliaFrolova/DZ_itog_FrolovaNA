c5a63d8b19012b90b70d542f32fdbf50949e997a
Natalia Frolova <n.smirnova76@mail.ru>
2022-10-02 22:33:27 +0300

    created two arrays and a cycle

--- Program.cs
+++ Program.cs
@@ -1,1 +1,8 @@
 ﻿string[] array = { "hello", "2", "world", ":-)", "1234", "1567", "-2", "Russia", "Denmark", "Kazan" };
+string[] result = new string[new Random().Next(0, 4)];
+Random rnd = new Random();
+
+for (int i = 0; i < result.Length; i++) 
+{
+result[i] = array[rnd.Next(array.Length)];
+}
